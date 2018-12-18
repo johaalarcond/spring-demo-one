ff24bc0ade5911f88810d189b8afdd00a54fbe77
Johanna Alarcon <johaalarcond@gmail.com>
2018-12-18 09:17:55 -0500

    Commit init

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		//previous
+		//Coach theCoach = new BaseballCoach();
+		// create the object
+		Coach theCoach = new TrackCoach();
+		
+		//use the object
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
