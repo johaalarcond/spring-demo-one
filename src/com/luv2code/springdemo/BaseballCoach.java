a855004c3170416f0f0d02d66ff5901c815be073
Johanna Alarcon <johaalarcond@gmail.com>
2018-12-18 10:03:08 -0500

    dependency injection

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -2,6 +2,15 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach{
 	
+	//define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	//define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService){ 
+		fortuneService = theFortuneService;
+	}
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting pratice";
@@ -9,8 +18,8 @@ public class BaseballCoach implements Coach{
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// use  my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 }
